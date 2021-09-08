a24d1312fdc3866d70f14847f34cef671484e03d
Anastasios Andronidis <anastasios.andronidis@gmail.com>
2021-09-08 18:00:49 +0300

    Avoid re-downloading article URLs

--- scraper.py
+++ scraper.py
@@ -1,7 +1,10 @@
 import logging
+import os.path as path
 import requests
 
 from bs4 import BeautifulSoup
+
+DB_FILENAME = "all_article_urls.txt"
 
 logging.basicConfig(format="%(message)s", level=logging.INFO)
 
@@ -32,7 +35,7 @@ def get_all_article_urls(page):
     )
 
 
-def main():
+def scrap_article_urls():
     root_url = "https://www.ellinikahoaxes.gr/category/kathgories/"
     logging.info(f"Root page: {root_url}")
 
@@ -45,9 +48,28 @@ def main():
         page = get_next_page(page)
 
     logging.info(f"Total articles collected: {len(a_urls)}")
+    return a_urls
 
-    with open("all_article_urls.txt", "w") as outfile:
-        outfile.write("\n".join(a_urls))
+
+def save_article_urls(a_urls):
+    with open(DB_FILENAME, "w") as f:
+        f.write("\n".join(a_urls))
+
+
+def load_article_urls():
+    with open(DB_FILENAME, "r") as f:
+        return f.read().splitlines()
+
+
+def main():
+    db_path = path.join(path.dirname(path.realpath(__file__)), DB_FILENAME)
+    if not path.isfile(db_path):
+        logging.info(f"Looking for article URLs")
+        a_urls = scrap_article_urls()
+        save_article_urls(a_urls)
+    else:
+        a_urls = load_article_urls()
+        logging.info(f"Article URLs found in DB: {len(a_urls)}")
 
 
 if __name__ == "__main__":
