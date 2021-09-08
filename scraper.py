04548349a6e0a475daf82b59609117d980b35928
Anastasios Andronidis <anastasios.andronidis@gmail.com>
2021-09-08 17:45:27 +0300

    Get article URLs

--- scraper.py
+++ scraper.py
@@ -23,14 +23,28 @@ def get_next_page(page):
         return None
 
 
+def get_all_article_urls(page):
+    return list(
+        map(
+            lambda tag: tag.get("href"),
+            page.find_all("a", attrs={"class": "btn btn-secondary"}),
+        )
+    )
+
+
 def main():
     root_url = "https://www.ellinikahoaxes.gr/category/kathgories/"
     logging.info(f"Root page: {root_url}")
 
     page = download_page(root_url)
 
+    a_urls = []
     while page:
+        a_urls += get_all_article_urls(page)
+
         page = get_next_page(page)
+
+    logging.info(f"Total articles collected: {len(a_urls)}")
 
 
 if __name__ == "__main__":
